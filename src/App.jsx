e8442ea4dcd3335f8498f656918ac49b7d8e64ae
tanjiro5849 <ghostbo5849tgh@gmail.com>
2026-01-16 15:43:44 +0545

    Fix routing and imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Rooms from "./pages/Rooms";
@@ -9,22 +10,28 @@ import Reservation from "./pages/Reservation";
 import Messages from "./pages/Messages";
 import Calender from "./pages/Calender";
 import Review from "./pages/Review";
+import HousekeepingDashboard from "./pages/Housekeeping";
+import Inventory from "./pages/Inventory";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Login runs first */}
+        {/* Login */}
         <Route path="/" element={<Login />} />
 
-        {/* After login */}
+        {/* Main Pages */}
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/rooms"  element={<Rooms />} />
+        <Route path="/rooms" element={<Rooms />} />
         <Route path="/reservation" element={<Reservation />} />
         <Route path="/messages" element={<Messages />} />
-        <Route path="/calender" element={<Calender/>} />
+        <Route path="/calender" element={<Calender />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/housekeeping" element={<HousekeepingDashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+
+        {/* 404 */}
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-        <Route path="/review" element={<Review/>} />
       </Routes>
     </BrowserRouter>
   );
