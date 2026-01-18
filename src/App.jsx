8759a2725e73b229b8ff24eca90c3b0eccaf0536
tanjiro5849 <ghostbo5849tgh@gmail.com>
2026-01-18 20:38:08 +0545

    Update main app structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import "./index.css";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Rooms from "./pages/Rooms";
+import Reservation from "./pages/Reservation";
+import Messages from "./pages/Messages";
+import Calender from "./pages/Calender";
+import Review from "./pages/Review";
+import HousekeepingDashboard from "./pages/Housekeeping";
+import Inventory from "./pages/Inventory";
+import Finances from "./pages/Finances";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      {/* <NavBar/> */}
+      <Routes>
+        {/* Login */}
+        <Route path="/" element={<Login />} />
+
+        {/* Main Pages */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/calender" element={<Calender />} />
+        <Route path="/reviews" element={<Review />} />
+        <Route path="/housekeeping" element={<HousekeepingDashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/finances" element={<Finances />} />
+
+        {/* 404 */}
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
